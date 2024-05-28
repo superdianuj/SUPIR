29aae85c68ea7e3aa19dd478aa6d7e6abd0474ec
Spider Man <junaid@kau.kr>
2024-05-28 15:35:43 +0900

    Add files via upload

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
-LLAVA_CLIP_PATH = 'opt/LLaVA1.5/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = 'opt/LLaVA1.5 /llava-v1.5-13b'
-SDXL_CLIP1_PATH = 'opt/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = 'opt/CLIP-ViT-bigG-14-laion2B-39B-b160k'
+LLAVA_CLIP_PATH = 'my_models/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = 'my_models/llava-v1.5-13b'
+SDXL_CLIP1_PATH = 'my_models/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = 'my_models/CLIP-ViT-bigG-14-laion2B-39B-b160k'
