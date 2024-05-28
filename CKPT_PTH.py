e76999350fc86eed81fdf0d6d739f677e2084b74
Spider Man <junaid@kau.kr>
2024-05-28 16:22:09 +0900

    Add files via upload

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
 LLAVA_CLIP_PATH = 'my_models/clip-vit-large-patch14-336'
 LLAVA_MODEL_PATH = 'my_models/llava-v1.5-13b'
 SDXL_CLIP1_PATH = 'my_models/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = 'my_models/CLIP-ViT-bigG-14-laion2B-39B-b160k'
+SDXL_CLIP2_CKPT_PTH = 'my_models/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
